baf9b2536a6fa0d567456731433071a9f5972d11
Anirudh Sharma <anisharma34@gmail.com>
2026-09-14 17:49:50 -0400

    #1: Added spring project to backend folder.

---
+++ backend/ohio-air-analysis/src/main/java/com/github/asharma1999/ohio_air_analysis/OhioAirAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.github.asharma1999.ohio_air_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhioAirAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhioAirAnalysisApplication.class, args);
+	}
+
+}
---
+++ backend/ohio-air-analysis/src/test/java/com/github/asharma1999/ohio_air_analysis/OhioAirAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.asharma1999.ohio_air_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhioAirAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
